ae661700c71b54ddbfc5e68a62c014aaecdbd340
BBDUYU <tmdwhddh@gmail.com>
2026-02-09 12:03:09 +0900

    questbook

---
+++ sb08_mjt_guestbook/src/main/java/org/doit/ik/Sb08MjtGuestbookApplication.java
@@ -0,0 +1,13 @@
+package org.doit.ik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb08MjtGuestbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sb08MjtGuestbookApplication.class, args);
+	}
+
+}
