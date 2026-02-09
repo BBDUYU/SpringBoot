418967fe112c5783a3ab6c5d4b23c9265876f974
BBDUYU <tmdwhddh@gmail.com>
2026-02-09 12:01:00 +0900

    jsp

---
+++ sb01_jsp/src/main/java/org/doit/ik/controller/BoardController.java
@@ -0,0 +1,17 @@
+package org.doit.ik.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.log4j.Log4j2;
+
+@Controller
+@Log4j2
+@RequestMapping("/board")
+public class BoardController {
+	@GetMapping("/list")
+	public void list() {
+		log.info("@ @ @ BoardController.index()");
+	}
+}
