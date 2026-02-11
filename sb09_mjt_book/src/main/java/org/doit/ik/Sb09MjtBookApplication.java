43d450c0622d30ef56f8171b8839d53a9831eee1
BBDUYU <tmdwhddh@gmail.com>
2026-02-11 09:46:16 +0900

    연관관계

---
+++ sb09_mjt_book/src/main/java/org/doit/ik/Sb09MjtBookApplication.java
@@ -0,0 +1,13 @@
+package org.doit.ik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb09MjtBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sb09MjtBookApplication.class, args);
+	}
+
+}
