0ada4e5c3300dca6fec6ff0c289b36c0d3df996b
BBDUYU <tmdwhddh@gmail.com>
2026-02-11 09:36:44 +0900

    Delete sb08_mjt_guestbook directory

--- sb08_mjt_guestbook/src/main/java/org/doit/ik/guestbook/repository/GuestbookRepository.java
+++
@@ -1,9 +0,0 @@
-package org.doit.ik.guestbook.repository;
-
-import org.doit.ik.guestbook.entity.Guestbook;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.querydsl.QuerydslPredicateExecutor;
-
-public interface GuestbookRepository extends JpaRepository<Guestbook, Long>,QuerydslPredicateExecutor<Guestbook>{
-	
-}
