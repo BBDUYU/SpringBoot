782dfea24939e514c1aa9c0c34a2a25f941bbdfd
BBDUYU <tmdwhddh@gmail.com>
2026-02-12 10:23:57 +0900

    board

--- sb09_mjt_book/src/test/java/org/doit/ik/Sb09MjtBoardApplicationTests.java
+++ sb09_mjt_book/src/test/java/org/doit/ik/Sb09MjtBoardApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Sb09MjtBookApplicationTests {
+class Sb09MjtBoardApplicationTests {
 
 	@Test
 	void contextLoads() {
