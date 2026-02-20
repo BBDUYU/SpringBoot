0fa066a422e70ed1f8bc2b11961ddce0fa66ec2e
BBDUYU <tmdwhddh@gmail.com>
2026-02-20 10:33:44 +0900

    DTO구현

---
+++ sb11_hjt_sbb2/src/main/java/org/doit/ik/sbb/question/QuestionForm.java
@@ -0,0 +1,11 @@
+package org.doit.ik.sbb.question;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class QuestionForm { //DTO
+	private String subject;
+	private String content;
+}
