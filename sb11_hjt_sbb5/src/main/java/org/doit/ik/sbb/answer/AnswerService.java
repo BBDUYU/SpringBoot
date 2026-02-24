2d0c49c46ef8e93daa7767a3bf845479deec050f
BBDUYU <tmdwhddh@gmail.com>
2026-02-24 10:38:06 +0900

    앵커

--- sb11_hjt_sbb5/src/main/java/org/doit/ik/sbb/answer/AnswerService.java
+++ sb11_hjt_sbb5/src/main/java/org/doit/ik/sbb/answer/AnswerService.java
@@ -17,6 +17,7 @@ public class AnswerService {
    private final AnswerRepository answerRepository;
    
    // [1] 답변 등록
+   /*
    public void create(Question question, String content,SiteUser author) {
       Answer answer = new Answer();
       answer.setContent(content);
@@ -25,6 +26,17 @@ public class AnswerService {
       answer.setAuthor(author);
       this.answerRepository.save(answer);
    }
+   */
+   // [2] 답변등록 + 앵커기능
+   public Answer create(Question question, String content,SiteUser author) {
+	      Answer answer = new Answer();
+	      answer.setContent(content);
+	      answer.setCreateDate(LocalDateTime.now());
+	      answer.setQuestion(question);
+	      answer.setAuthor(author);
+	      this.answerRepository.save(answer);
+	      return answer;
+	   }
    public Answer getAnswer(Integer id) {
        Optional<Answer> answer = this.answerRepository.findById(id);
        if (answer.isPresent()) {
